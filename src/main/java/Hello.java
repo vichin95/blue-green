180cac40c0a8fb86be07008913d96beb0e8d9030
vichin95 <148530524+vichin95@users.noreply.github.com>
2024-01-29 19:46:00 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,7 +9,7 @@ import javax.servlet.http.*;
 // Extend HttpServlet class
 public class Hello extends HttpServlet {
 
-  public static String appVersion = "3";
+  public static String appVersion = "2";
   public static String welcomeMsg = "Welcome to the Hello World App";
 
    private String message;
